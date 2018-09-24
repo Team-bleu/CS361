a4150a57169f315a00d8d7a03049fcec166c8ae1
xmr1996 <xiangm@uwm.edu>
2018-09-23 20:14:26 -0500

    lab3

---
+++ lab3/main.py
@@ -0,0 +1,59 @@
+import unittest
+
+from Rational import Rational
+
+rational1 = Rational(2,1)
+rational2 = Rational(3,1)
+rational3 = Rational(4,1)
+rational4 = Rational(1,3)
+rational5 = Rational(2,3)
+
+#print(rational1.__dict__)
+#print(rational2.__dict__)
+
+class MainTests(unittest.TestCase):
+  def test_init(self):
+
+    self.assertEqual(rational3, 4/1)
+    self.assertEqual(rational2, 3/1)
+    self.assertEqual(rational4, 1/3)
+
+  def test_add(self):
+  
+    self.assertEqual(rational1 + rational2, 5)
+    self.assertEqual(rational1 + rational3, 6)
+  
+  def test_sub(self):
+    
+    self.assertEqual(rational2 - rational1, 1)
+    self.assertEqual(rational3 - rational1, 2)
+ 
+  def test_mul(self):
+    
+    self.assertEqual(rational1 * rational2, 6)
+    self.assertEqual(rational1 * rational3, 8)
+  def test_div(self):
+    
+    self.assertEqual(rational2 / rational1, 3/2)
+    self.assertEqual(rational3 / rational1, 2)
+
+  def test_str(self):
+    
+    self.assertEqual(str(rational2), "3/1")
+    self.assertEqual(str(rational1), "2/1")
+    self.assertEqual(str(rational3), "4/1")
+
+  def test_float(self):
+    
+    self.assertEqual(float(rational1), 2.0)
+    self.assertEqual(float(rational2), 3.0)
+    self.assertEqual(float(rational3), 4.0)
+
+#print("kkkkkkk")
+suite = unittest.TestSuite()
+suite.addTest(unittest.makeSuite(MainTests))
+runner = unittest.TextTestRunner()
+res=runner.run(suite)
+print(res)
+print("*"*20)
+for i in res.failures: print(i[1])
